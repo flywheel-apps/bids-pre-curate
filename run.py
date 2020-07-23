587aae5ac83f4d23a3547e0a726886cfb2b97173
Nate Richman <nate.rich2@gmail.com>
2020-07-23 14:49:27 -0500

    MAIN: Moved api requests for generating CSV to run.py
    Moved the api requests to run.py so that the csv generation functions
    can be unit tested.

--- run.py
+++ run.py
@@ -4,7 +4,6 @@
 import sys
 import pandas as pd
 
-
 import flywheel_gear_toolkit
 from utils import bids_pre_curate
 from utils.bids.run_level import get_run_level_and_hierarchy
@@ -13,6 +12,8 @@ from utils.bids.run_level import get_run_level_and_hierarchy
 def parse_config(context):
     conf_dict = {}
     config = context.config
+    if not config:
+        return
     if config.sessions_per_subject:
         conf_dict['ses_per_sub'] = config.sessions_per_subject
     else:
@@ -50,28 +51,30 @@ def validate_inputs(context):
 
 
 def main(gtk_context):
-
-    hierarchy = get_run_level_and_hierarchy(
-        gtk_context.client, gtk_context.destination["id"]
-    )
-    group = hierarchy.group
-    project_label = hierarchy.project_label
+    #    hierarchy = get_run_level_and_hierarchy(
+    #        gtk_context.client, gtk_context.destination["id"]
+    #    )
+    group = "scien"  # hierarchy.group
+    project_label = "Nate"  # hierarchy.project_label
 
     config = parse_config(gtk_context)
     inputs = validate_inputs(gtk_context)
 
+    project = gtk_context.client.lookup(f'{group}/{project_label}')
     if inputs:
-        project = gtk_context.client.lookup(f'{group}/{project_label}')
         acq_df = pd.read_csv(inputs[0])
         ses_df = pd.read_csv(inputs[1])
         sub_df = pd.read_csv(inputs[2])
         bids_pre_curate.read_from_csv(acq_df, sub_df, ses_df, project)  # ,config.dry_run)
     else:
-        bids_pre_curate.build_csv(gtk_context.client, group, project_label)
+        fw = gtk_context.client
+        acqs = [acq.to_dict() for acq in fw.get_project_acquisitions(project.id)]
+        sess = [ses.to_dict() for ses in fw.get_project_sessions(project.id)]
+        subs = [sub.to_dict() for sub in fw.get_project_subjects(project.id)]
+        bids_pre_curate.build_csv(acqs, subs, sess, project.label)
 
 
 if __name__ == "__main__":
-
     gtk_context = flywheel_gear_toolkit.GearToolkitContext()
 
     # Setup basic logging and log the configuration for this job
