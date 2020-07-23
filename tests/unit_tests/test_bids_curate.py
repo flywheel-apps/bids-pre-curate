dd66660c0874f971df5f2de527c03b5804365f92
Nate Richman <nate.rich2@gmail.com>
2020-07-23 15:54:42 -0500

    ENH: Added testing function for data2csv.
    Added unit test for data2csv.

--- tests/unit_tests/test_bids_curate.py
+++ tests/unit_tests/test_bids_curate.py
@@ -1,10 +1,48 @@
-import unittest
+from utils.bids_pre_curate import data2csv
+def test_data2csv_normal_data():
+    data = [{'age': None,
+         'analyses': None,
+         'code': 'sub-13',
+         'cohort': None,
+         'created': '',
+         'ethnicity': None,
+         'files': [],
+         'firstname': None,
+         '_id': '5db0845e69d4f3002d16ee05',
+         'info': {},
+         'info_exists': False,
+         'label': 'sub-13',
+         'lastname': None,
+         'master_code': None,
+         'modified': '',
+         'notes': [],
+         'parents': {'acquisition': None,
+                     'analysis': None,
+                     'group': 'scien',
+                     'project': '5db0759469d4f3001f16e9c1',
+                     'session': None,
+                     'subject': None},
+         'permissions': {},
+         'project': '5db0759469d4f3001f16e9c1',
+         'race': None,
+         'revision': 17,
+         'sex': None,
+         'species': None,
+         'strain': None,
+         'tags': [],
+         'type': None}]
+    proj_label = 'test_proj'
+    keep_keys = [['_id', 'label'],
+                ['_id', ['parents', 'group'], 'label'],
+                ['_id', ['parents', 'subject'], 'label']]
+    column_renames = [['id', 'existing_acquisition_label'],
+                    ['id', 'subject_label', 'existing_session_label'],
+                    ['id', 'subject_label', 'existing_session_label']]
+    prefixes = ['acq','sub','sess']
+    user_columns = [[],['test'],['test1','test2']]
+    for keep_key, column_rename, prefix, user_column in zip(keep_keys,column_renames, prefixes, user_columns):
+        path,df = data2csv(data,proj_label,prefix, column_rename,user_column, no_print=True)
+        print(path)
+        print(df)
+test_data2csv_normal_data()
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
