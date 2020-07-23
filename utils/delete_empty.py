8b2b3a00061d60162f92b6c3df2a57b31373176d
Nate Richman <nate.rich2@gmail.com>
2020-07-23 16:33:43 -0500

    ENH: Updated delete_empty.
    Updated documentation for delete_empty, and made it check both children
    sessions and attached files, instead of just children sessions.

--- utils/delete_empty.py
+++ utils/delete_empty.py
@@ -1,36 +1,36 @@
 import flywheel
 import logging
+
 fw = flywheel.Client('', root=True)
 
 log = logging.getLogger(__name__)
 
 log.info(fw.get_config().site.api_url)
 
- def delete_empty_subject(subject_id):
-     """Deletes a subject after confirming its empty.
 
-     A subject will be empty if it has no sessions attached to it,
-     this function checks whether the given subject has any children
-     sessions.  If it doesn't, it deletes the subject and returns
-     true, if it does, it returns false and logs a warning
+def delete_empty_subject(subject_id, dry_run):
+    """Deletes a subject after confirming its empty.
 
-     Args:
-         subject_id (str): the subject id
+    A subject will be empty if it has no sessions attached to it,
+    this function checks whether the given subject has any children
+    sessions.  If it doesn't, it deletes the subject and returns
+    true, if it does, it returns false and logs a warning
 
-     Returns:
-        boolean
+    Args:
+        subject_id (str): the subject id
 
-     Raises:
-         ValueError: if the subject is not empty
-     """
-     subject = fw.get_subject(subject_id)
-     sessions = subject.sessions
-     if len(sessions.find()):
-         log.warning('Subject not empty: sessions still attached. Not deleting')
-         return False
-     elif len(subject.files) > 0:
-         log.warning('Subject not empty: files still attached.  Not deleting')
-         return False
-     else:
-         fw.delete_subject(subject.id)
-         return True
+    Returns:
+       boolean: whether the subject was deleted or not
+
+    """
+    subject = fw.get_subject(subject_id)
+    sessions = subject.sessions
+    if len(sessions.find()):
+        log.warning('Subject not empty: sessions still attached. Not deleting')
+        return False
+    elif len(subject.files) > 0:
+        log.warning('Subject not empty: files still attached.  Not deleting')
+        return False
+    else:
+        fw.delete_subject(subject.id)
+        return True
