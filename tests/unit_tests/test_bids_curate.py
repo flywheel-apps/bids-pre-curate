d88dee4c8b33fe612cd1296c6dca290c4b9d70ff
Nate Richman <nate.rich2@gmail.com>
2020-07-24 10:16:55 -0500

    ENH: Added test for keep_specified_keys
    Added a test for keep_specified_keys and expanded test data slightly.

--- tests/unit_tests/test_bids_curate.py
+++ tests/unit_tests/test_bids_curate.py
@@ -1,5 +1,6 @@
 from utils.bids_pre_curate import data2csv
 from utils.deep_dict import nested_get
+from utils.bids_pre_curate import keep_specified_keys
 test_data = [{'age': None,
          'analyses': None,
          'code': 'sub-13',
@@ -20,7 +21,46 @@ test_data = [{'age': None,
                      'analysis': None,
                      'group': 'scien',
                      'project': '5db0759469d4f3001f16e9c1',
-                     'session': None,
+                     'session': {
+                         'session_no': 1,
+                         'session_info': None
+                     },
+                     'subject': None},
+         'permissions': {
+             'perm-01': 'nate-has-access'
+         },
+         'project': '5db0759469d4f3001f16e9c1',
+         'race': None,
+         'revision': 17,
+         'sex': None,
+         'species': None,
+         'strain': None,
+         'tags': [],
+         'type': None},
+        {'age': None,
+         'analyses': None,
+         'code': 'sub-14',
+         'cohort': None,
+         'created': '',
+         'ethnicity': None,
+         'files': [],
+         'firstname': None,
+         '_id': '5db0845e69d4f3002d16ee05',
+         'info': {},
+         'info_exists': False,
+         'label': 'sub-14',
+         'lastname': 'hello',
+         'master_code': None,
+         'modified': '',
+         'notes': [],
+         'parents': {'acquisition': None,
+                     'analysis': None,
+                     'group': 'Nate',
+                     'project': '5db0759469d4f3001f16e9c1',
+                     'session': {
+                         'session_no': None,
+                         'session_info': None
+                     },
                      'subject': None},
          'permissions': {},
          'project': '5db0759469d4f3001f16e9c1',
@@ -31,6 +71,8 @@ test_data = [{'age': None,
          'strain': None,
          'tags': [],
          'type': None}]
+
+
 
 
 def test_data2csv_normal_data():
@@ -76,8 +118,33 @@ def test_nested_get():
         assert val == result
         print(f'expected: {result}, got: {val}, passed')
 
+def test_keep_specified_keys():
+    keep_keys = [['_id', 'label','lastname'],
+                 ['_id', ['parents', 'group'], 'label'],
+                 ['_id', ['permissions','perm-01'], 'label'],
+                 [['parents','subject'],['parents','session','session_no'],
+                    ['parents','session','session_info']]]
+    expected = [[{'_id': '5db0845e69d4f3002d16ee05', 'label': 'sub-13', 'lastname': None},
+                 {'_id': '5db0845e69d4f3002d16ee05', 'label': 'sub-14', 'lastname': 'hello'}],
+                [{'_id': '5db0845e69d4f3002d16ee05', 'parents.group': 'scien', 'label': 'sub-13'},
+                 {'_id': '5db0845e69d4f3002d16ee05', 'parents.group': 'Nate', 'label': 'sub-14'}],
+                [{'_id': '5db0845e69d4f3002d16ee05', 'permissions.perm-01': 'nate-has-access', 'label': 'sub-13'},
+                 {'_id': '5db0845e69d4f3002d16ee05', 'permissions.perm-01': None, 'label': 'sub-14'}],
+                [{'parents.subject': None, 'parents.session.session_no': 1,
+                    'parents.session.session_info': None},
+                 {'parents.subject': None, 'parents.session.session_no': None,
+                    'parents.session.session_info': None}]]
+
+    for keys, expected in zip(keep_keys, expected):
+        kept = keep_specified_keys(test_data, keys)
+        for kept_dict,exp in zip(kept,expected):
+            assert type(kept_dict) is dict
+            assert kept_dict == exp
+
+
 #def test_keep_specified_keys():
 #   ['']
 test_nested_get()
+test_keep_specified_keys()
 test_data2csv_normal_data()
 
