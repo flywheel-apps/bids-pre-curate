4141b421375e99fa7a763e550083b2ca543aa6fa
Nate Richman <naterichman@flywheel.io>
2020-07-27 13:09:29 -0500

    ENH: Integration tests for phase 2.

--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -22,13 +22,19 @@ gtk_context.init_logging()
 log = gtk_context.log
 log.info('Test')
 
-
+project = gtk_context.client.lookup('scien/Nate-BIDS-pre-curate')
 inputs = run.validate_inputs(gtk_context)
 
-acq_df = pd.read_csv(inputs[0], index_col=0)
-ses_df = pd.read_csv(inputs[1], index_col=0)
-sub_df = pd.read_csv(inputs[2], index_col=0)
+acq_df = pd.read_csv(inputs[0]).fillna('')
+ses_df = pd.read_csv(inputs[1]).fillna('')
+sub_df = pd.read_csv(inputs[2]).fillna('')
+
 
 print(acq_df)
 print(ses_df)
 print(sub_df)
+
+#utils.bids_pre_curate.read_from_csv(acq_df,sub_df,ses_df,project,dry_run=True)
+#utils.bids_pre_curate.handle_acquisitions(acq_df,gtk_context.client)
+utils.bids_pre_curate.move_and_delete_subjects(sub_df,ses_df,project,gtk_context.client,dry_run=True)
+
