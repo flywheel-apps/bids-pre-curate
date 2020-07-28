dcb43ff836fb714f2821fcbb3880c2f54e2af18c
Nate Richman <nate.rich2@gmail.com>
2020-07-28 11:37:06 -0500

    ENH: Modified logging and script organization to work better.

--- tests/integration_tests/test.sh
+++ tests/integration_tests/test.sh
@@ -1,59 +1,73 @@
 #!/bin/bash
-
+# Pull project creation script
+git submodule update
+cwd=$(pwd)
+PYTHONPATH=$cwd:$PYTHONPATH
+echo "$PYTHONPATH"
 GEAR_DIR=/tmp/gear
+# Create requirements
+pipenv run pip freeze > tests/integration_tests/requirements.txt
+# Remove local requirements
+sed -i '/^-e/d' tests/integration_tests/requirements.txt
+# Install requirements
+pip install --user -r tests/integration_tests/requirements.txt
 
-rm -rf $GEAR_DIR
-cp -r ../../ $GEAR_DIR
+sudo rm -rf $GEAR_DIR
+cp -r . $GEAR_DIR
 function build_container {
   # Create gear directory and copy resources to dir
 
   #
   cd $GEAR_DIR
   mkdir -p $GEAR_DIR/flywheel/v0
-  mv {run.py,utils,Pipfile,Pipfile.lock} flywheel/v0
+  cp {setup.py,run.py,Pipfile,Pipfile.lock} flywheel/v0
+  cp -r utils flywheel/v0
 
   cd flywheel/v0
 
   if [[ "$2" == "--test" ]]; then
-    mv $GEAR_DIR/tests/integration_tests/test_run.py test_run.py
+    cp $GEAR_DIR/tests/integration_tests/test_run.py test_run.py
     chmod +x test_run.py
-    mv $GEAR_DIR/tests/integration_tests/manifest.json manifest.json
-    mv $GEAR_DIR/tests/integration_tests/Dockerfile Dockerfile
+    cp $GEAR_DIR/tests/integration_tests/manifest.json manifest.json
+    cp $GEAR_DIR/tests/integration_tests/Dockerfile Dockerfile
+    cp $GEAR_DIR/tests/integration_tests/requirements.txt requirements.txt
     docker build -t flywheel/bids-pre-curate:$1  ./
   else
-    mv $GEAR_DIR/Dockerfile Dockerfile
-    mv $GEAR_DIR/manifest.json manifest.json
+    cp $GEAR_DIR/Dockerfile Dockerfile
+    cp $GEAR_DIR/manifest.json manifest.json
+    cp $GEAR_DIR/requirements.txt requirements.txt
     docker build -t flywheel/bids-pre-curate:$1 ./
   fi
 }
 
 ##################### Unit Testing
-pipenv run python ../unit_tests/test_bids_curate.py
+python -m pytest tests/unit_tests/test_bids_curate.py
 
 ## Pre stage 1, clean and make new project
-yes y | pipenv run python delete_project.py --group scien --project Nate-BIDS-pre-curation --data-only
-pipenv run python ../BIDS_popup_curation/makesession.py --group scien --project Nate-BIDS-pre-curate --subjects IVA_202,IVA_202-1,IVA_202-2
+
+#yes y | python tests/integration_tests/delete_project.py --group scien --project Nate-BIDS-pre-curation --data-only
+#python tests/BIDS_popup_curation/makesession.py --group scien --project Nate-BIDS-pre-curate --subjects IVA_202,IVA_202-1,IVA_202-2
 
 ##################### Stage 1 integration testing
 # Build container as production
 build_container $1
 cd $GEAR_DIR/flywheel/v0
-pipenv install pandas
 # Run local csv generation stage of gear
 fw gear local
 # Outputs are
-pipenv run python ../../tests/integration_tests/populate_csv.py --sub-name IVA_202  \
-  --acquisitions output/acquisition_labels_Nate-BIDS-pre-curate.csv \
-  --subjects output/subject_codes_Nate-BIDS-pre-curate.csv \
-  --sessions output/session_labels_Nate-BIDS-pre-curate.csv
-
-
+cd $GEAR_DIR
+mkdir input
+cp flywheel/v0/output/* input/
+python tests/integration_tests/populate_csv.py --sub-name IVA_202  \
+  --acquisitions input/acquisition_labels_Nate-BIDS-pre-curate.csv \
+  --subjects input/subject_codes_Nate-BIDS-pre-curate.csv \
+  --sessions input/session_labels_Nate-BIDS-pre-curate.csv
 
 ##################### Stage 2 integration testing
 build_container $1 --test
 cd $GEAR_DIR/flywheel/v0
 
-fw gear local --acquisition_table $GEAR_DIR/output/acquisition_labels_Nate-BIDS-pre-curate.csv \
-  --session_table $GEAR_DIR/output/session_labels_Nate-BIDS-pre-curate.csv \
-  --subject_table $GEAR_DIR/output/subject_codes_Nate-BIDS-pre-curate.csv
+fw gear local --acquisition_table ../../input/acquisition_labels_Nate-BIDS-pre-curate.csv \
+  --session_table ../../input/session_labels_Nate-BIDS-pre-curate.csv \
+  --subject_table ../../input/subject_codes_Nate-BIDS-pre-curate.csv
 
--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -24,12 +24,14 @@ log.info('Test')
 
 project = gtk_context.client.lookup('scien/Nate-BIDS-pre-curate')
 inputs = run.validate_inputs(gtk_context)
+log.info('Validated inputs')
 
 acq_df = pd.read_csv(inputs[0]).fillna('')
 ses_df = pd.read_csv(inputs[1]).fillna('')
 sub_df = pd.read_csv(inputs[2]).fillna('')
 
 
+log.info('Finished loading data')
 #print(acq_df)
 #print(ses_df)
 #print(sub_df)
@@ -38,8 +40,11 @@ sub_df = pd.read_csv(inputs[2]).fillna('')
 def test():
     #test_bids_curate.run()
     utils.bids_pre_curate.handle_acquisitions(acq_df,gtk_context.client,dry_run=True)
+    log.info('Finished handle_acquisitions')
     utils.bids_pre_curate.move_and_delete_subjects(sub_df,ses_df,project,gtk_context.client,dry_run=True)
+    log.info('Finished move_and_delete_subjects')
     utils.bids_pre_curate.read_from_csv(acq_df,sub_df,ses_df,project)
+    log.info('Finished read_from_csv')
 test()
 
 
