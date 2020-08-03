2be8d83cf342d52acf67459f6676e2b813b56163
Nate Richman <naterichman@flywheel.io>
2020-08-03 14:36:40 -0500

    BUG: Change from property access to dictionary access b/c I always forget that.

--- run.py
+++ run.py
@@ -90,7 +90,7 @@ def main(gtk_context):
         sess = [ses.to_dict() for ses in fw.get_project_sessions(project.id)]
         subs = [sub.to_dict() for sub in fw.get_project_subjects(project.id)]
 
-        file_names = bids_pre_curate.build_csv(acqs, subs, sess, project.label,allows=config.allows)
+        file_names = bids_pre_curate.build_csv(acqs, subs, sess, project.label,allows=config['allows'])
         if not os.path.exists(gtk_context.output_dir):
             try:
                 os.mkdir(gtk_context.output_dir)
