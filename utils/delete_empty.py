6f9cffd03e0dee1fceeb9f9f87e240f03629ab75
Nate Richman <naterichman@flywheel.io>
2020-08-05 11:34:40 -0500

    MAIN: Added documentation for delete_empty_subjects.

--- utils/delete_empty.py
+++ utils/delete_empty.py
@@ -9,10 +9,11 @@ log = logging.getLogger(__name__)
 def delete_empty_subject(subject_id, dry_run,fw):
     """Deletes a subject after confirming its empty.
 
-    A subject will be empty if it has no sessions attached to it,
-    this function checks whether the given subject has any children
-    sessions.  If it doesn't, it deletes the subject and returns
-    true, if it does, it returns false and logs a warning
+    A subject will be empty if it has no sessions attached to it, and
+    if it has no files attached to it. This function checks whether
+    the given subject has any children sessions or files.  If it
+    doesn't, it deletes the subject and returns true, if it does,
+     it returns false and logs a warning
 
     Args:
         subject_id (str): the subject id
