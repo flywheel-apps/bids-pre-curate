4dfd65c8d27533ee804e204eea837a0f1d3b757c
Nate Richman <naterichman@flywheel.io>
2020-08-12 09:04:04 -0500

    MAIN: Add sys path append, remove unused tests.

--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -6,10 +6,12 @@ import json
 import logging
 import os
 import shutil
+import sys
 from pathlib import Path
 from pprint import pprint
 import pytest
 
+sys.path.append(str(Path(__file__).parents[2].resolve()))
 
 import flywheel_gear_toolkit
 import pandas as pd
@@ -22,18 +24,7 @@ import utils.bids_pre_curate
 
 
 
-def test():
-    #test_bids_curate.run()
-    utils.bids_pre_curate.handle_acquisitions(acq_df,gtk_context.client,project,dry_run=True)
-    log.info('Finished handle_acquisitions')
-    utils.bids_pre_curate.handle_sessions(ses_df, gtk_context.client, project, dry_run=True)
-    log.info('Finished handle_sessions')
-    utils.bids_pre_curate.handle_subjects(sub_df, gtk_context.client, project, dry_run=True)
-    log.info('Finished handle_subjects')
 
-    utils.bids_pre_curate.read_from_csv(acq_df,sub_df,ses_df,project)
-    log.info('Finished read_from_csv')
-test()
 
 
 
@@ -45,4 +36,8 @@ class TestWetRun:
     def test_wet_run(self, gtk_context):
         run.main(gtk_context)
 
-        if gtk_context.config
+#        if gtk_context.config:
+
+if __name__ == '__main__':
+    gtk_context = flywheel_gear_toolkit.GearToolkitContext()
+    run.main(gtk_context)
