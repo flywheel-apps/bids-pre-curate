678fec83f5a6c7e701bd3184bcee3b38ecfedc84
Nate Richman <naterichman@flywheel.io>
2020-08-12 10:16:38 -0500

    ENH: Added make_file_name_safe for automatic CSV population.

--- tests/integration_tests/populate_csv.py
+++ tests/integration_tests/populate_csv.py
@@ -1,5 +1,8 @@
 import argparse
-
+import re
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parents[2].resolve()))
 import pandas as pd
 from utils.fly.make_file_name_safe import make_file_name_safe
 from numpy.random import randint
@@ -12,18 +15,19 @@ def main(args):
 
     # Generate random binary for rows in acquisition df
     # Randomly, or intermittently populate values
-    randoms = randint(2,size=acq_df.shape[0])
+    randoms = randint(2, size=acq_df.shape[0])
     for i, row in acq_df.iterrows():
-        acq_df.at[i, 'new_acquisition_label'] = \
-            f"{make_file_name_safe(row['existing_acquisition_label'],'','')}"
+        acq_df.at[i, 'new_acquisition_label'] =  \
+            f"{make_file_name_safe(row['existing_acquisition_label'], re.compile('[^A-Za-z0-9]'), '')}"
+
         acq_df.at[i, 'modality'] = (f'T1{i}'
                                     if not randoms[i] else '')
         acq_df.at[i, 'task'] = (f'task{i}'
                                 if i % 3 == 0 else '')
         acq_df.at[i, 'run'] = (f'{i}'
-                                if i % 4 == 0 else '')
+                               if i % 4 == 0 else '')
         acq_df.at[i, 'ignore'] = ('yes'
-                                if i % 5 == 0 else '')
+                                  if i % 5 == 0 else '')
 
     for i, row in sub_df.iterrows():
         sub_df.at[i, 'new_subject_label'] = (args.sub_name
