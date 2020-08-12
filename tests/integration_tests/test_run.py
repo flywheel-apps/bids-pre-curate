a4013d32cd6bff68945241257d327afdb9be6b05
Nate Richman <naterichman@flywheel.io>
2020-08-12 10:36:18 -0500

    ENH: Added integration testing using pytest for stage 1 and stage 2

--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -34,7 +34,25 @@ def gtk_context():
 
 class TestWetRun:
     def test_wet_run(self, gtk_context):
-        run.main(gtk_context)
+        acq = gtk_context.get_input_path('acquisition_table')
+        ses = gtk_context.get_input_path('session_table')
+        sub = gtk_context.get_input_path('subject_table')
+        if acq and ses and sub:
+            # All inputs provided, there should be no outputs
+            run.main(gtk_context)
+            outdir = Path(gtk_context.output_dir)
+            assert len(os.listdir(outdir)) == 0
+        elif not (acq or ses or sub):
+            # No inputs provided, there should be three outputs
+            run.main(gtk_context)
+            outdir = Path(gtk_context.output_dir)
+            outfiles = os.listdir(outdir)
+            assert len(outfiles) == 3
+        else:
+            # Raise system exit when not all three or none are provided
+            with pytest.raises(SystemExit):
+                run.main(gtk_context)
+                assert True
 
 #        if gtk_context.config:
 
