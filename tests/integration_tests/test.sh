2035a1f7828527f21bc53170ea2577c491df393e
Nathan Richman <naterichman@flywheel.io>
2020-08-13 10:22:31 -0500

    ENH: Made all tests run inside the docker container so dependencies are resolved.

--- tests/integration_tests/test.sh
+++ tests/integration_tests/test.sh
@@ -16,8 +16,8 @@ if [[ ! -a tests/integration_tests/requirements.txt ]]; then
   pip install --user -r tests/integration_tests/requirements.txt
 fi
 
-function build_and_run_container {
-  VERSION=$(grep "version" manifest.json | sed "s/[^0-9._]//g")
+VERSION=$(grep "version" manifest.json | sed "s/[^-0-9._]//g")
+function build_container {
   echo "$(pwd)"
 
   if [[ ! -d $GEAR ]]; then
@@ -26,63 +26,80 @@ function build_and_run_container {
   # Create gear directory and copy resources to dir
   cp -r {setup.py,run.py,Pipfile*,tests,utils} $GEAR/
 
-  # Replace config.json with second argument
-  if [[ -f "$2" ]]; then
-    cp "$2" $GEAR/tests/assets/config.json
-  fi
-
   cd $GEAR
 
   mv tests/integration_tests/Dockerfile .
 
-  if [[ "$3" == '--hide-build' ]]; then
+  if [[ ! -z "$1"  && "$1" == '--hide-build' ]]; then
     docker build -t flywheel/bids-pre-curate:$VERSION ./ > /dev/null 2>&1
   else
     docker build -t flywheel/bids-pre-curate:$VERSION ./
   fi
-  mkdir inputs
-  mkdir outputs
+  if [[ ! -d inputs ]]; then
+    mkdir inputs
+  fi
+  if [[ ! -d outputs ]]; then
+    mkdir outputs
+  fi
+}
+function run_container {
 
+  cd $GEAR
   container=$(docker container ls -a | grep bids-pre-curate)
   if [[ ! -z $container ]]; then
     docker container rm bids-pre-curate
   fi
 
   echo "Running command inside container: $1"
-  docker run -it --rm --name bids-pre-curate \
-    -v $GEAR/inputs:/flywheel/v0/input \
-    -v $GEAR/outputs:/flywheel/v0/output \
-    -v $GEAR/tests/assets/config.json:/flywheel/v0/config.json \
-    -v $GEAR/tests/integration_tests/user.json:/root/.config/flywheel/user.json \
-    flywheel/bids-pre-curate:$VERSION "$1" 2>&1
+  # If config file supplied run the container as a gear
+  if [[ -f "$2" ]]; then
+    docker run -it --rm --name bids-pre-curate \
+      -v $GEAR/inputs:/flywheel/v0/input \
+      -v $GEAR/outputs:/flywheel/v0/output \
+      -v "$GEAR/$2":/flywheel/v0/config.json \
+      -v $GEAR/tests/integration_tests/user.json:/root/.config/flywheel/user.json \
+      flywheel/bids-pre-curate:$VERSION "$1" 2>&1
+  else
+    #Otherwise, run as just an image
+    docker run -it --rm --name bids-pre-curate \
+      -v $GEAR/inputs:/flywheel/v0/input \
+      -v $GEAR/outputs:/flywheel/v0/output \
+      -v $GEAR/tests/integration_tests/user.json:/root/.config/flywheel/user.json \
+      flywheel/bids-pre-curate:$VERSION "$1" 2>&1
+  fi
 }
 
 function unit_test {
   ##################### Unit Testing
-  pipenv run python3 -m pytest  tests/unit_tests/test_bids_pre_curate.py
+  run_cmd="pipenv run python -m pytest tests/unit_tests/test_bids_pre_curate.py"
+  run_container "$run_cmd"
 }
 function pre_stage_1 {
   ## Pre stage 1, clean and make new project
-  cd "$cwd"
-  yes y | pipenv run python3 tests/integration_tests/delete_project.py --group "$GROUP" --project "$PROJECT" --data-only
-  pipenv run python3 tests/BIDS_popup_curation/makesession.py --group "$GROUP" --project "$PROJECT" --subjects IVA_202,IVA_202-1,IVA_202-2
+  run_cmd="yes y | pipenv run python3 tests/integration_tests/delete_project.py \
+                      --group $GROUP --project $PROJECT --data-only \
+                   && pipenv run python3 tests/BIDS_popup_curation/makesession.py \
+                      --group $GROUP --project $PROJECT --subjects IVA_202,IVA_202-1,IVA_202-2"
+  run_container "$run_cmd"
 }
 function stage_1 {
   ##################### Stage 1 integration testing
   # Build container as production
   cd "$cwd"
 #  run="python3 -m pdb tests/integration_tests/test_run.py"
-  build_and_run_container "$1" tests/assets/config-stage1.json
+  run_container "$1" tests/assets/config-stage1.json
 }
 
 function populate_csv {
   # Outputs are
   cd "$GEAR"
-  mv outputs/* inputs
-  pipenv run python3 tests/integration_tests/populate_csv.py --sub-name IVA_202  \
-    --acquisitions inputs/acquisition_labels_$PROJECT.csv \
-    --subjects inputs/subject_codes_$PROJECT.csv \
-    --sessions inputs/session_labels_$PROJECT.csv
+  mv -f outputs/* inputs
+  run_cmd="pipenv run python3 tests/integration_tests/populate_csv.py --sub-name IVA_202 \
+    --acquisitions input/acquisition_labels_$PROJECT.csv \
+    --subjects input/subject_codes_$PROJECT.csv \
+    --sessions input/session_labels_$PROJECT.csv"
+  run_container "$run_cmd"
+
 }
 
 function stage_2 {
@@ -90,7 +107,7 @@ function stage_2 {
 
   cd $cwd
 #  run="python3 -m pdb tests/integration_tests/test_run.py"
-  build_and_run_container "$1" tests/assets/config-stage2.json
+  run_container "$1" tests/assets/config-stage2.json
 }
 __test_usage="
   ... test {unit_test,stage1,stage2,all} [opts]
@@ -102,11 +119,11 @@ __test_usage="
 "
 test(){
   if [[ "$2" == "-c" ]] || [[ "$2" == "--with-cov" ]]; then
-    cmd="python3 -m pipenv run pytest --cov run tests/integration_tests/test_run.py"
+    cmd="pipenv run python -m pytest --cov run tests/integration_tests/test_run.py"
   elif [[ "$2" == "-d" ]] || [[ "$2" == "--with-debug" ]]; then
-    cmd="python3 -m pipenv run pytest --pdb run tests/integration_tests/test_run.py"
+    cmd="pipenv run python -m pytest --pdb tests/integration_tests/test_run.py"
   else
-    cmd="python3 -m pipenv run pytest run tests/integration_tests/test_run.py"
+    cmd="pipenv run python -m pytest tests/integration_tests/test_run.py"
   fi
 
   case $1 in
@@ -137,7 +154,7 @@ __shell_usage="
     Enter bash shell in container
 "
 shell(){
-  build_and_run_container "/bin/bash"
+  run_container "/bin/bash"
 }
 __run_usage="
   ... run {full,pdb} [opts]
@@ -151,8 +168,8 @@ __run_usage="
     -t. --stage-two   Run stage 2 test
 "
 run(){
-  run_cmd="python3 tests/integration_tests/test_run.py"
-  pdb_cmd="python3 -m pdb tests/integration_tests/test_run.py"
+  run_cmd="pipenv run python3 tests/integration_tests/test_run.py"
+  pdb_cmd="pipenv run python3 -m pdb tests/integration_tests/test_run.py"
   if [[ $1 == 'pdb' ]]; then
     cmd=$pdb_cmd
     shift
@@ -202,6 +219,7 @@ __usage="
   "$__shell_usage"
   "
 main() {
+  build_container
   while [ $# -gt 0 ]; do
     case "$1" in
       "run")
@@ -216,7 +234,7 @@ main() {
         ;;
       "shell")
         shift
-        build_and_run_container "/bin/bash"
+        shell
         break
         ;;
       "-h" | "--help")
